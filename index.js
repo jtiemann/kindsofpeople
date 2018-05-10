b60dc73716b1a6cdf7aff69a540a9fdfb0827d3c
jtiemann <jtiemann@digitalpersonae.com>
2018-05-10 15:42:55 -0400

    minor refactor: optimize matrix compare, comments

--- index.js
+++ index.js
@@ -47,23 +47,13 @@ const Binary     = Symbol()
 const Decimal    = Symbol()
 const Neither    = Symbol()
 
-const MatrixToXY = Mat => {
-  let Jim = []
-  for (i=0;i<NumRows;i++)
-    for (j=0;j<NumCols;j++) {
-      Jim.push([i, j, Mat[i][j]])
-  }
-  return Jim
-}
 const MatrixBaseNormalizer = ATest => [ATest[0][0]-1, ATest[0][1]-1, ATest[1][0]-1, ATest[1][1]-1]
-const TheMatrix = MatrixToXY(Matrix)
 const AddUniquely = (Arr, Unit) => {
                       Arr.push(Unit) 
                       let pf = new Set(Arr)
                       return Array.from(pf)
 }
 
-//const xyvEq = (Matrx, 
 
 /////////////////////
 // LOGIC FUNCTIONS //
@@ -72,23 +62,22 @@ const AddUniquely = (Arr, Unit) => {
 const CanIMove = (Matrix) => (Loc) => (Dir) => {
  let isMoveOK
   switch (Dir) {
-    case North: isMoveOK = Matrix.filter((x)=>JSON.stringify([Loc[0]-1,Loc[1], Loc[2]]) === JSON.stringify(x))
+    case North: isMoveOK = Loc[0]+1 <  NumRows && Matrix[Loc[0]+1][Loc[1]] === Loc[2] ? [[Loc[0]+1,Loc[1], Loc[2]]] : []
                 break; 
-    case South: isMoveOK = Matrix.filter((x)=>JSON.stringify([Loc[0]+1,Loc[1], Loc[2]]) === JSON.stringify(x))
+    case South: isMoveOK = Loc[0]-1 >= 0       && Matrix[Loc[0]-1][Loc[1]] === Loc[2] ? [[Loc[0]-1,Loc[1], Loc[2]]]: []
                 break; 
-    case East:  isMoveOK = Matrix.filter((x)=>JSON.stringify([Loc[0],Loc[1]+1, Loc[2]]) === JSON.stringify(x))
+    case East:  isMoveOK = Loc[1]+1 <  NumCols && Matrix[Loc[0]][Loc[1]+1] === Loc[2] ? [[Loc[0],Loc[1]+1, Loc[2]]]: []
                 break; 
-    case West:  isMoveOK = Matrix.filter((x)=>JSON.stringify([Loc[0],Loc[1]-1, Loc[2]]) === JSON.stringify(x))
+    case West:  isMoveOK = Loc[1]-1 >= 0       && Matrix[Loc[0]][Loc[1]-1] === Loc[2] ? [[Loc[0],Loc[1]-1, Loc[2]]]: []
                 break; 
   }
   return isMoveOK
 }
  
-const CanIMoveXYDir = CanIMove(TheMatrix)
+const CanIMoveXYDir = CanIMove(Matrix)
 const MovesFromHere = function(Here) {
-  let IsInMatrix = TheMatrix.filter((x) => JSON.stringify(Here) === JSON.stringify(x)).length > 0 
+  let IsInMatrix = Matrix[Here[0]][Here[1]] === Here[2] 
   if (!IsInMatrix) return [[],[],[],[]]
-
   let CanIMoveDir = CanIMoveXYDir(Here)
   return [North, South, East, West].map((D) => CanIMoveDir(D))
 }
@@ -110,18 +99,46 @@ const ForkMe     = (PSteps, NextStop, Forks) =>
 ////////////
 
 const run = (tests) => tests.map((ATest) => {
+  /*
+   initialize 
+   --internal structures,
+   --normalize start/end coords 
+     (1,1) -> (0,0)
+  */
     var Visited = [];
     var Forks   = [];
     let [StartX, StartY, EndX, EndY] = MatrixBaseNormalizer(ATest)
-
-    // REDUCE [0,1] -> "Binary" | "Decimal" | "Neither"
+  /*
+   first try Decimal then try Binary, default output is Neither
+   REDUCE [0,1] -> "Binary" | "Decimal" | "Neither"
+  */ 
     let Ans = [0,1].reduce((acc, BOrD) => {
+  /*
+   Am I there yet?
+  */
       if (IsArrived([StartX, StartY, BOrD], BOrD, EndX, EndY)) return BOrD ? "Decimal" : "Binary" 
+  /* 
+   can I move from here? If I can then take a step, else this test with current kind (0/1) failed. 
+  */      
       var PSteps =  UnvisitedMovesFromHere(Visited)(MovesFromHere([StartX, StartY, BOrD]))
       Visited.push([StartX, StartY, Matrix[StartX][StartY] ]) 
+
       var NextStop = HasPStep(PSteps) ? FindPSteps(PSteps) : null
       if (NextStop == null) return acc 
+  /* 
+    now cycle through: step
+                           ->have I arrived ? return 
+                                            : ->take step if can ? update forks (cells with multiple possible steps) 
+                                                                 : pop a fork try taking a step
+  */
+  
+  /*
+  walking = (nextstop) => {
+     [nextstop].map((nxt) => {Visited = AddUniquely(Visited, NextStop)); return nxt}
+               .map()...
 
+  }
+  */
       while (NextStop) {
         Visited = AddUniquely(Visited, NextStop)
         if (IsArrived(NextStop, BOrD, EndX, EndY)) return BOrD ? "Decimal" : "Binary" 
@@ -145,7 +162,7 @@ console.log(run(TestLines).join("\n"))
 ///////////////////////
 // LOGGING FUNCTIONS //
 ///////////////////////
-assert(run(TestLines).join(" ") == "Binary Decimal Neither", "correct")
+assert(run(TestLines).join(" ") == "Binary Decimal Neither", "outcome: ")
 
 //console.log("NumCols = ", NumCols)  
 //console.log("NumRows = ", NumRows)  
@@ -155,7 +172,7 @@ console.log(Matrix)
 //console.log("MatrixToXY = ", TheMatrix)  
 function assert(value, desc) {
   if (true) {
-    console.log(value ? 'pass: ' + desc : 'fail: ' + desc);
+    console.log(value ? desc + 'passed! '  :   desc + 'failure...');
   }
   else{
     return "";
